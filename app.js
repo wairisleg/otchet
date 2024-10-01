d93c6b935b67fa957bd4b273be88158393af3e92
wairisleg <dombobra666@gmail.com>
2024-10-01 17:02:23 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const giftDate = new Date();
 giftDate.setDate(giftDate.getDate() + 108);
 
+// Проверяем, сохранена ли дата подарка в localStorage
+if (!localStorage.getItem('giftDate')) {
+    localStorage.setItem('giftDate', giftDate.getTime());
+}
+
+// Получаем дату подарка из localStorage
+const storedGiftDate = new Date(parseInt(localStorage.getItem('giftDate'), 10));
+
 function updateCountdown() {
     const now = new Date();
-    const timeRemaining = giftDate - now;
+    const timeRemaining = storedGiftDate - now;
 
     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -15,6 +23,7 @@ function updateCountdown() {
     if (timeRemaining < 0) {
         clearInterval(interval);
         document.getElementById('countdown').innerHTML = "Подарок уже здесь!";
+        localStorage.removeItem('giftDate'); // Удаляем дату подарка из localStorage
     }
 }
 
